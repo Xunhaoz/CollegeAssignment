bb1fe18648e6032f261b79b979b3d9c6955de5a3
Xunhaoz <leo20020529@gmail.com>
2021-12-13 06:56:23 +0800

    assignment-11

--- c++-assignment-11/A11-110502528-2.cpp
+++ c++-assignment-11/A11-110502528-2.cpp
@@ -9,56 +9,52 @@ Course 2021-CE1003-A
 #include<vector>
 using namespace std;
 vector<char> processed_string;
-vector<int> radius;
+vector<int> record;
 int main(){
-    string temp;
+    string user_input;
     while(1){
-        if(!(cin >> temp) && temp == "-1")
-            break;
-        //初始化
-        radius.clear();
-        processed_string.clear();
-        radius.push_back(0);
-        radius.push_back(0);
+        cin >> user_input;
+        if(user_input == "-1") break;
 
-        //預處理
-        processed_string.push_back('*');
-        processed_string.push_back('_');
-        for(int i=0;i<temp.size();++i){
-            processed_string.push_back(temp[i]);
+        processed_string.clear();
+        record.clear();
+        processed_string.push_back('#');
+        for(int i=0;i<user_input.size();++i){
             processed_string.push_back('_');
+            processed_string.push_back(user_input[i]);
         }
+        processed_string.push_back('_');
         processed_string.push_back('&');
 
-        //Manacher's Algorithm
-        int c=0, r=0, l=0, max_index=0 ;
+        int l=0, r=-1, pos=0;
+        record.push_back(0);
+        record.push_back(0);
         for(int i=2;i<processed_string.size();++i){
-            cout<<processed_string[i];
-            int mirror_index = c * 2 - i;
+            if(i>r) record.push_back(1);
+            else record.push_back(min(record[l+r-i],r-i+1));
 
-            //回文串長度是否大於大回文串
-            if(mirror_index<=2 || i>=r || i+radius[mirror_index]>=r){
+            while(processed_string[i-record[i]] == processed_string[i+record[i]])
+                record[i]++;
 
-                if(mirror_index>=0 && i+radius[mirror_index]>=r) radius.push_back(r-i);
+            record[i]--;
 
-                else radius.push_back(0);
-
-                while(processed_string[i-radius[i]-1] == processed_string[i+radius[i]+1])
-                    radius[i]++;
-
-                c = i;
-                l = i - radius[i];
-                r = i + radius[i];
+            if(i+record[i]>r){
+                l = i-record[i];
+                r = i+record[i];
             }
-            else radius.push_back(radius[mirror_index]);
 
-            if(radius[i] > radius[max_index])
-                max_index = i;
+            if(record[i]>record[pos])
+                pos = i;
         }
 
-        for(int i=(max_index-radius[max_index])/2;i<(max_index-radius[max_index])/2+radius[max_index];i++)
-            cout << processed_string[i];
-        cout << radius[max_index] << endl;
+        if(record[pos] == 1)
+            cout << "Palindrome not existed!"<<endl<<endl;
+        else{
+            cout<<"Palindrome:";
+            for(int i = pos-record[pos]+1;i<pos+record[pos];i+=2)
+                cout<< processed_string[i];
+            cout<<endl<<"Length:"<<record[pos]<<endl<<endl;
+        }
 
 
     }
