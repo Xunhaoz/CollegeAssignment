6ed45ed836a1833a361679b8d5b1cf0eb49fd053
Xunhaoz <leo20020529@gmail.com>
2021-12-18 05:42:08 +0800

    practice 12

---
+++ c++-parctice-12/P12-110502528.cpp
@@ -0,0 +1,38 @@
+/*
+Practice 12
+Name: ±i³Ôµq
+Student Number: 110502528
+Course 2021-CE1003-A
+*/
+#include    <iostream>
+
+using namespace std;
+
+void add(float ax, float ay, float bx, float by, float *cx, float *cy) {
+    *cx = ax + bx;
+    *cy = ay + by;
+}
+
+void scale(float ax, float ay, double s, float *cx, float *cy) {
+    *cx = ax * s;
+    *cy = ay * s;
+}
+
+int main() {
+    float ax, ay, bx, by, cx, cy, s;
+
+    cout << "A: ";
+    cin >> ax >> ay;
+    cout << "B: ";
+    cin >> bx >> by;
+    cout << "s: ";
+    cin >> s;
+
+    add(ax, ay, bx, by, &cx, &cy);
+    cout << "A + B = " << "(" << cx << ", " << cy << ")" << endl;
+
+    scale(ax, ay, s, &cx, &cy);
+    cout << "A * s = " << "(" << cx << ", " << cy << ")" << endl;
+
+    return 0;
+}
